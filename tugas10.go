74ac340a5f91447ebe557589e583860a2ee3a72e
Cryptious <cryptiousw@gmail.com>
2022-01-18 13:10:32 +0700

    first commit

---
+++ tugas10.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Martino")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Coding")
+	time.Sleep(time.Second * 5)
+	fmt.Println("18 Maret 97")
+	time.Sleep(time.Second * 2)
+	fmt.Println("SELESAI")
+
+}
